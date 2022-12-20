29aa7cdcd105eec00c79c5bd872b1a180bd30b66
orlyomyim <yinpiaowongtrakunme@cmu.ac.th>
2022-12-20 16:38:41 +0700

    ข้อ3

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,19 +1,17 @@
-#include<iostream>
-using namespace std;
-
+//ไม่ต้องเติม function main()
 char before(char x){
-	//Write your function definition here
-}
+if(x >= 65 and x <= 90 ){
+ x = x-1 ;
+    if(x < 65){
+        x = 90 ;
+        return x ;
+        }
+    return x ;
+    }
+else{
+    return '0' ;
+    }
 
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+   
+    
 }
